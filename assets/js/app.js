89e1a2a3c2fb676152d5e5b72f267498e1e7029d
Margot <margoterudy@gmail.com>
2021-12-22 13:33:43 -0600

    margot chart2

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,10 @@
-//Chart size
+//Chart Set up
+var svgWidth = 960;
+var svgHeight = 500;
 
-const chartWidth = 600;
-const chartHeight = 400;
-
-    const margin = {
-    top: 30,
-    right: 50,
-    bottom: 90,
-    left: 100
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 50
 };
- 
-
-
-// Calculate chart width and height
-const width = chartWidth - margin.left - margin.right;
-const height = chartHeight - margin.top - margin.bottom;
-
-
-// Create  wrapper, append group
-
-const svg = d3.select("#scatter")
-            .append("svg")
-            .attr("width", chartWidth)
-            .attr("height", chartHeight);
-
-
