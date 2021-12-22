46ab9db5c3a9ae9d916f43948f9a9636b1531a5b
Margot <margoterudy@gmail.com>
2021-12-22 13:57:47 -0600

    SVG wrapper, append an SVG group that will hold our chart

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
-//Chart Set up
-var svgWidth = 960;
-var svgHeight = 500;
+//Chart set up (found in D3 activity 9 Hairmetal)
+var chartWidth = 960;
+var chartHeight = 500;
 
 var margin = {
   top: 20,
@@ -8,3 +8,18 @@ var margin = {
   bottom: 60,
   left: 50
 };
+
+var width = chartWidth - margin.left - margin.right;
+var height = chartHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+var svg = d3
+  .select("#scatter")
+  .append("svg")
+  .attr("width", chartWidth)
+  .attr("height", chartHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+  // Import Data - CSV - still activity 9
