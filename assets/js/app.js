c7c805f706a61bb82ad1d45238a9a11989ac6793
Margot <margoterudy@gmail.com>
2021-12-22 14:10:13 -0600

    add csv, parse data and check console

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,4 +22,13 @@ var svg = d3
 var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-  // Import Data - CSV - still activity 9
+  // Import Data - CSV -*still activity 9
+  d3.csv("assets/data/data.csv").then(function(USCensusData) {
+    
+    // Step 1: Parse Data/Cast as numbers
+    USCensusData.forEach(function(data) {
+      data.age = +data.age;
+      data.smokes = +data.smokes;
+    //   console.log(data); yay data shows!!!
+    });
+})
